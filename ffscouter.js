4108d3ed1c3890ec2ba1d0e29b64d632d22b359d
Jason Chu <jchu@xentac.net>
2025-12-23 10:01:32 +1300

    Fix spacing in settings summary when expanded

--- ffscouter.js
+++ ffscouter.js
@@ -175,6 +175,10 @@ if (!singleton) {
                 border-radius: 5px;
             }
 
+            .ff-settings-accordion div.ff-settings-body {
+              margin-top: 10px;
+            }
+
             .ff-settings-header {
                 display: flex;
                 justify-content: space-between;
@@ -2215,6 +2219,7 @@ if (!singleton) {
 
     // Create main content div
     const content = document.createElement("div");
+    content.className = "ff-settings-body";
 
     // API Key Explanation
     const apiExplanation = document.createElement("div");
