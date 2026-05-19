b95ef6abbdb2ef2f3d6e8a68f0901bc718be6193
Jason Chu <jchu@xentac.net>
2026-05-19 15:20:19 -0700

    Process faction pages even on navigation changes
    Not just page loads

--- src/features/faction/index.ts
+++ src/features/faction/index.ts
@@ -53,13 +53,71 @@ const apply_ff_members_list = (root: HTMLElement = document.body) => {
   }
 };
 
+const process_page = () => {
+  wait_for_element(".members-list", 10_000).then((node) => {
+    if (node instanceof HTMLElement) {
+      logger.debug("Found members-list!");
+      monitor_member_list(node);
+    }
+  });
+
+  wait_for_element(".chain-attacks-list", 10_000).then((node) => {
+    if (node instanceof HTMLElement) {
+      logger.debug("Found chain-attacks-list!");
+      monitor_member_list(node, true);
+    }
+  });
+
+  // TODO: Support #faction-info and #faction-main swapping on personal faction page
+
+  wait_for_element("#faction_war_list_id", 10_000).then(async (node) => {
+    if (!node) {
+      return;
+    }
+    logger.debug("Found faction_war_list_id");
+    const descriptions_observer = new MutationObserver(async (mutations) => {
+      for (const mutation of mutations) {
+        for (const node of mutation.addedNodes) {
+          if (
+            node instanceof HTMLElement &&
+            node.classList.contains("descriptions")
+          ) {
+            logger.debug(
+              "Observed mutation that included adding descriptions",
+              node,
+            );
+            const faction_war = await wait_for_element(".faction-war", 10_000);
+            if (faction_war instanceof HTMLElement) {
+              monitor_member_list(faction_war, true);
+            }
+          }
+        }
+      }
+    });
+    descriptions_observer.observe(node, { childList: true });
+    logger.debug("Set up descriptions observer on", node);
+
+    const existing_descriptions = node.querySelector(".descriptions");
+    if (existing_descriptions) {
+      const faction_war = await wait_for_element(
+        ".faction-war",
+        10_000,
+        existing_descriptions,
+      );
+      if (faction_war instanceof HTMLElement) {
+        apply_ff_members_list(faction_war);
+      }
+    }
+  });
+};
+
 export default {
   name: "Faction page FF display",
   description: "Shows FF arrows on both your faction and other faction pages.",
   executionTime: StartTime.DocumentBody,
 
   async shouldRun() {
-    // Run on the attack page
+    // Run on the faction pages
     return (
       torn_page("factions", { step: "profile" }) ||
       torn_page("factions", { step: "your" })
@@ -67,64 +125,11 @@ export default {
   },
 
   async run() {
-    wait_for_element(".members-list", 10_000).then((node) => {
-      if (node instanceof HTMLElement) {
-        logger.debug("Found members-list!");
-        monitor_member_list(node);
-      }
+    window.navigation.addEventListener("navigate", () => {
+      process_page();
     });
 
-    wait_for_element(".chain-attacks-list", 10_000).then((node) => {
-      if (node instanceof HTMLElement) {
-        logger.debug("Found chain-attacks-list!");
-        monitor_member_list(node, true);
-      }
-    });
-
-    // TODO: Support #faction-info and #faction-main swapping on personal faction page
-
-    wait_for_element("#faction_war_list_id", 10_000).then(async (node) => {
-      if (!node) {
-        return;
-      }
-      logger.debug("Found faction_war_list_id");
-      const descriptions_observer = new MutationObserver(async (mutations) => {
-        for (const mutation of mutations) {
-          for (const node of mutation.addedNodes) {
-            if (
-              node instanceof HTMLElement &&
-              node.classList.contains("descriptions")
-            ) {
-              logger.debug(
-                "Observed mutation that included adding descriptions",
-                node,
-              );
-              const faction_war = await wait_for_element(
-                ".faction-war",
-                10_000,
-              );
-              if (faction_war instanceof HTMLElement) {
-                monitor_member_list(faction_war, true);
-              }
-            }
-          }
-        }
-      });
-      descriptions_observer.observe(node, { childList: true });
-      logger.debug("Set up descriptions observer on", node);
-
-      const existing_descriptions = node.querySelector(".descriptions");
-      if (existing_descriptions) {
-        const faction_war = await wait_for_element(
-          ".faction-war",
-          10_000,
-          existing_descriptions,
-        );
-        if (faction_war instanceof HTMLElement) {
-          apply_ff_members_list(faction_war);
-        }
-      }
-    });
+    process_page();
   },
 
   httpIntercept: {
--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,1 +1,4 @@
 declare const __FF_SCOUTER_V2_VERSION__: string;
+interface Window {
+  navigation?: any;
+}
