b9896370d7fb8235301b6e2e7c02db7072969116
Jason Chu <jchu@xentac.net>
2026-05-19 15:26:27 -0700

    Fix incomplete dom monitoring code

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -235,12 +235,14 @@ export class MonitorElements {
   private observer: MutationObserver;
 
   private timer: ReturnType<typeof setInterval> | null = null;
+  private timeout: number;
 
   constructor(
     node_matcher: NodeMatcherFn,
     handler: HandlerFn,
     root: HTMLElement,
     continuous: boolean,
+    timeout: number,
   ) {
     this.node_matcher = node_matcher;
     this.handler = handler;
