2228c0a73cb75f3d77ce4f2a5764ab9336d6af43
Jason Chu <jchu@xentac.net>
2026-05-27 22:04:04 -0700

    Change hash check for profile page to make it apply more reliably

--- src/features/faction/index.ts
+++ src/features/faction/index.ts
@@ -973,8 +973,14 @@ export function should_run_faction(): boolean {
   if (torn_page("factions", { step: "profile" })) {
     return true;
   }
-  if (torn_page("factions", { step: "your" }, ["", "#", "#/", "#/tab=info"])) {
-    return true;
+  if (torn_page("factions", { step: "your" })) {
+    if (
+      window.location.hash === "#/" ||
+      window.location.hash.startsWith("#/war/") ||
+      window.location.hash === "#/tab=info"
+    ) {
+      return true;
+    }
   }
   return false;
 }
